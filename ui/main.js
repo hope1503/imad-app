2046a984fb5aac684e51594137b5b893ce4f89a2
hope1503 <30770305+hope1503@users.noreply.github.com>
2017-09-05 01:03:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,14 +8,14 @@ button.onclick = function() {
 
    // Capture the response and store it in a variable
    request.onreadystatechange = function() {
-   if (request.readyState === XMLHttpRequest.DONE) {
-   //Take some action
-   if (request.status === 200) {
-       var counter = request.responseText;
-       var span = document.getElementById('count');
-       span.innerHTML = counter.toString();
-    }
-    }
+     if (request.readyState === XMLHttpRequest.DONE) {
+         //Take some action
+         if (request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+         }
+     }
    //Not done yet
    };
    
@@ -25,29 +25,29 @@ button.onclick = function() {
 };
 
   //submit name
-   var nameInput = document.getElementById('name');
-   var name = nameInput.value;
-   var submit = document.getElementById('submit_btn');
-   submit.onclick = function () {
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
   
   //Create a request object
    var request = new XMLHttpRequest();
   
   // Capture the response and store it in a variable
    request.onreadystatechange = function() {
-   if (request.readyState === XMLHttpRequest.DONE) {
-   //Take some action
-   if (request.status === 200) {
-   //Capture a list of names and render it as a list
-   var names = ['name1', 'name2', 'name3', 'name4'];
-   var list = '';
-   for (var i=0; i<names.length; i++) {
-       list += '<li>' + names[i] + '</li>';
-   }
-   var ul = document.getElementById('namelist');
-   ul.innerHTML = list;
-    }
-    }
+     if (request.readyState === XMLHttpRequest.DONE) {
+         //Take some action
+          if (request.status === 200) {
+             //Capture a list of names and render it as a list
+             var names = ['name1', 'name2', 'name3', 'name4'];
+             var list = '';
+             for (var i=0; i<names.length; i++) {
+                  list += '<li>' + names[i] + '</li>';
+             } 
+             var ul = document.getElementById('namelist');
+             ul.innerHTML = list;
+          }
+     }
    //Not done yet
    };
    
