4db6fe26dd448b8908eb343f34b522ad5182830c
hope1503 <30770305+hope1503@users.noreply.github.com>
2017-09-04 23:40:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ button.onclick = function() {
       span.innerHTML = counter.toString();
       }
     }
-    //Not done yet
+   //Not done yet
    };
    
    // Make the request
@@ -24,3 +24,20 @@ button.onclick = function() {
    request.send(null);
 };
 
+  //Submit name
+   var nameInput = document.getElementById('name');
+   var name = nameInput.value;
+   var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  //Make a request to the server and send the name
+  
+
+  //Capture a list of names and render it as a list
+   var names = ['name1', 'name2', 'name3', 'name4'];
+   var list = '';
+   for (var i=0; i<names[i]; i++) {
+       list += '<li>' + names[i] + '</li>';
+   }
+   var ul = document.getElementById('namelist');
+   ul.innerHTML = list;
+};
