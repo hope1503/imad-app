4a0abc9a4489030f1af7c7bc80c3f80b44039185
hope1503 <30770305+hope1503@users.noreply.github.com>
2017-09-05 00:13:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,9 +80,19 @@ app.get('/counter', function (req,res) {
   res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function(req,res) {    //submit-name?name=xxxxxxxxx
+  // Get the name fom the request
+  var name = req.query.name;
+
+  names.push(name);
+  //JSON: Javascript Object Notation
+  res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName',function(req,res){
-//articleName==article-one
-//articles[articleName]=={} content object of article one
+  //articleName==article-one
+  //articles[articleName]=={} content object of article one
   var articleName= req.params.articleName;  
   res.send(createTemplate(articles[articleName]));
 });
@@ -97,16 +107,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names = [];
-app.get('/submit-name', function(req,res) {    //submit-name?name=xxxxxxxxx
-  // Get the name fom the request
-  var name = req.query.name;
-
-  names.push(name);
-  //JSON: Javascript Object Notation
-  res.send(JSON.stringify(names));
 });
 
 
