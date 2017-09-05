ed0d04af2bac6df9edbb6981b20d7fde9c4c0eae
hope1503 <30770305+hope1503@users.noreply.github.com>
2017-09-06 01:33:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ app.get('/hash/:input', function (req, res) {
 
 app.post('/create-user', function (req, res) {
    //username, password
+   // {"username": "Hemavathy", "password: "password"}
    //JSON
    var username = req.body.username;
    var password = req.body.password;
@@ -83,6 +84,31 @@ app.post('/create-user', function (req, res) {
     } else {
          res.send('User successfully created: ' + username);
    }
+  });
+});
+
+app.post('/login', function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+   
+  pool.query('SELECT * FROM "user" username = $1', function (err, result) {
+    if(err) {
+       res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length ===0) {
+           res.send(403).send('username/password is invalid');
+        } else {
+	       // Match the password
+	       var dbString = result.rows[0].password;
+           var salt = dbString.split('$')[2];
+	       var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+	       if (hashedPassword === dbString) {
+               res.send('Credentials correct!');
+           } else {
+	           res.send(403).send('Username/Password is invalid');
+           }
+        }   
+    }
   });
 });
 
