1f7cd244cf1fe800ed12496dbaeb4c1972c7d4ce
hope1503 <30770305+hope1503@users.noreply.github.com>
2017-09-05 15:15:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'rprhema',
+    database: 'rprhema',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    };
 
 var app = express();
 app.use(morgan('combined'));
@@ -72,6 +81,20 @@ function createTemplate (data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function (req,res) {
+  // Make a select request
+  // Return a response with the results
+  pool.query('SELECT * FROM test', function (err, result) {
+   if(err) {
+      res.status(500).send(err.toString());
+   } else {
+      res.send(JSON.stringify(result.rows));
+   }
+  });
+});
 });
 
 var counter = 0;
