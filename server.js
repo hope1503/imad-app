086b2f5cb25fa1d118326e299cbaa842a764cba7
hope1503 <30770305+hope1503@users.noreply.github.com>
2017-09-05 19:57:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,35 +14,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var articles={
-	'article-one' : {
-		title: 'Article One|Hemavathy',
-		heading: 'Article One',
-		date: 'August 8, Tuesday, 2017',
-		content: `<p>This is my content for the first article.This is my content for the first article. This is my content for the 			    	first article.This is my content for the first article.This is my content for the first article.
-	    	  	  </p>
-	    	  	  <p>This is my content for the first article.This is my content for the first article. This is my content for the 	
-	    	  	    first article.This is my content for the first article.This is my content for the first article.
-	    	 	  </p>
-	          	  <p>This is my content for the first article.This is my content for the first article. This is my content for the 	
-	          	    first article.This is my content for the first article.This is my content for the first article.
-	   	 	      </p>`
-		},
-	'article-two' : {
-		title: 'Article Two|Hemavathy',
-		heading: 'Article Two',
-		date: 'August 10, Thursday, 2017',
-		content:`<p>This is my content for the second article. Thank you sir and team for such a nice learning experience which I've 				never experienced before.
-	   	    	</p>`	 
-		},
-	'article-three' : {
-		title: 'Article Three|Hemavathy',
-		heading: 'Article Three',
-		date: 'August 15, Tuesday, 2017',
-		content:`<p>Happy learning!!!
-		    	</p>`
-		}
-};
 
 function createTemplate (data){
     var title= data.title;
