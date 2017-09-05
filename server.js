5a8834864c64daa0ea59cdbc75dc1e9a06e1e387
hope1503 <30770305+hope1503@users.noreply.github.com>
2017-09-05 20:16:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ function hash (input, salt) {
 app.get('/hash/:input', function (req, res) {
    var hashedString = hash(req.params.input, 'this-is-some-random-string');
    res.send(hashedString);
-};
+});
 
 var pool = new Pool(config);
 app.get('/test-db', function (req,res) {
