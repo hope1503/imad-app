44c35187e9f5f716aa9bdc1a706fd10f6b49a030
hope1503 <30770305+hope1503@users.noreply.github.com>
2017-09-05 16:00:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,9 +115,8 @@ app.get('/submit-name', function(req,res) {    //submit-name?name=xxxxxxxxx
 app.get('/articles/:articleName',function(req,res){
   //articleName==article-one
   //articles[articleName]=={} content object of article one
-  var articleName = req.params.articleName;
   
-  pool.query("SELECT * FROM article WHERE title ='" + req.params.articleName + " '", function (err, result) {
+  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
   if (err) {
       res.status(500).send(err.toString());
   } else {
