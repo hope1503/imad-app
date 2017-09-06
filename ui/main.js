56ce0dfdce07b22b6dcb59bc57a33184cf611ce4
hope1503 <30770305+hope1503@users.noreply.github.com>
2017-09-06 17:07:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,4 @@
-//Counter code
-var button = document.getElementById('counter');
-
-button.onclick = function() {
-
-   //Create a request object
-   var request = new XMLHttpRequest();
-
-   // Capture the response and store it in a variable
-   request.onreadystatechange = function() {
-     if (request.readyState === XMLHttpRequest.DONE) {
-         //Take some action
-         if (request.status === 200) {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-         }
-     }
-   //Not done yet
-   };
-   
-   // Make the request
-   request.open('GET', 'http://rprhema.imad.hasura-app.io/counter', true);
-   request.send(null);
-};
-
-  //submit name
+  //submit username/password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
   
@@ -51,10 +25,13 @@ submit.onclick = function () {
    };
    
    // Make the request
-   var nameInput = document.getElementById('name');
-   var name = nameInput.value;
-   request.open('GET', 'http://rprhema.imad.hasura-app.io/submit-name?name=' +name, true);
-   request.send(null);
+   var username = document.getElementById('username').value;
+   var password = document.getElementById('password').value;
+   console.log(username);
+   console.log(password);
+   request.open('POST', 'http://rprhema.imad.hasura-app.io/login', true);
+   request.setRequestHeader('Content-Type', 'application/json');
+   request.send(JSON.stringify({username: username, password: password}));
 };
 
   
