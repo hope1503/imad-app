a4f9c7ea9f8c14971d61590b68e5d40a4a000c04
hope1503 <30770305+hope1503@users.noreply.github.com>
2017-09-06 17:13:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,15 +10,11 @@ submit.onclick = function () {
      if (request.readyState === XMLHttpRequest.DONE) {
          //Take some action
           if (request.status === 200) {
-             //Capture a list of names and render it as a list
-             var names = request.responseText;
-             names = JSON.parse(names);
-             var list = '';
-             for (var i=0; i< names.length; i++) {
-                  list += '<li>' + names[i] + '</li>';
-             } 
-             var ul = document.getElementById('namelist');
-             ul.innerHTML = list;
+             alert('Logged in successfully');
+          } else if (request.status === 403) {
+             alert('Username/password is incorrect');
+          } else if (request.status === 500) {
+             alert('Something went wrong on the server');
           }
      }
    //Not done yet
